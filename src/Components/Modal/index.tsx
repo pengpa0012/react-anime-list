6f241421ec7ad08427c1e2f0b9fe84a668e86e9c
Pengpa0012 <peng.ilikha@gmail.com>
2022-09-29 18:55:36 +0800

    add description on modal

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -12,6 +12,7 @@ type Props = {
 
 function Modal({animeInfo, showModal, setShowModal, setAnimeInfo, id, ...props}: Props){ 
   const [hasData, setHasData] = useState<boolean | undefined>(undefined)
+  console.log(animeInfo)
 
   const getAnimeStaff = async () => {
     fetchAPI(`https://api.jikan.moe/v4/anime/${id}/staff`)
@@ -68,8 +69,11 @@ function Modal({animeInfo, showModal, setShowModal, setAnimeInfo, id, ...props}:
             : undefined
           }
           <div className="p-6">
-            <h3 className="text-2xl mb-2">{animeInfo?.info?.title}</h3>
-            <div className="flex">
+            <div className="mb-4">
+              <h3 className="text-2xl">{animeInfo?.info?.title_english || animeInfo?.info?.title}</h3>
+              <p className="text-sm">{animeInfo?.info?.title}</p>
+            </div>
+            <div className="flex my-8">
               <ul className="w-1/2">
                 <li className="my-1">Episodes: {animeInfo?.info?.episodes || "N/A"}</li>
                 <li className="my-1 flex">
@@ -83,8 +87,24 @@ function Modal({animeInfo, showModal, setShowModal, setAnimeInfo, id, ...props}:
                   </ul>
                 </li>
                 <li className="my-1">Type: {animeInfo?.info?.type}</li>
+                <li className="my-1">Aired: {animeInfo?.info?.aired?.string}</li>
+                <li className="my-1">Popularity: {animeInfo?.info?.popularity}</li>
+                <li className="my-1">Season: {animeInfo?.info?.season}</li>
               </ul>
               <ul className="w-1/2">
+                <li className="my-1">Status: {animeInfo?.info?.status}</li>
+                <li className="my-1 flex">
+                  Studios: 
+                  <ul>
+                    {
+                      animeInfo?.info?.studios?.map((item: any, i: number) => (
+                        <li className="inline first:ml-2 mr-1 text-sm" key={`studio-${i}`}>{item.name}</li>
+                      ))
+                    }
+                  </ul>
+                </li>
+                <li className="my-1">Rating: {animeInfo?.info?.rating}</li>
+                <li className="my-1">Year: {animeInfo?.info?.year}</li>
                 <li className="my-1">Score: {animeInfo?.info?.score}</li>
                 <li className="my-1">Source: {animeInfo?.info?.source}</li>
               </ul>
