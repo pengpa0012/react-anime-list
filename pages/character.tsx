76644e944a7008f20d7491352647ea772ed748a2
Pengpa0012 <peng.ilikha@gmail.com>
2022-10-25 19:56:03 +0800

    added favorites, nicknames

--- pages/character.tsx
+++ pages/character.tsx
@@ -6,11 +6,12 @@ import { Character } from '../src/Utilities/Types'
 function character() {
   const router = useRouter()
   const [character, setCharacter] = useState<Character>()
+  
   useEffect(() => {
     fetchAPI(`https://api.jikan.moe/v4/characters/${router.query.id}/full`)
     .then(response => setCharacter(response.data))
     .catch(console.error)
-  }, [router.query.id])
+  }, [router.isReady, router.query.id])
 
   return (
     <div className="container py-10">
@@ -20,6 +21,20 @@ function character() {
         </div>
         <div className="w-full">
           <h2 className="text-3xl mb-4">{character?.name}</h2>
+          <ul className="w-full mb-4">
+            <li className="my-1">Favorites: {character?.favorites || "N/A"}</li>
+            <li className="my-1 flex">
+              Nicknames: 
+              <ul className="flex flex-wrap items-center gap-1">
+                {
+                  character?.nicknames.length! > 0 ?
+                  character?.nicknames?.map((item: any, i: number) => (
+                    <li className="first:ml-2" key={`studio-${i}`}>{item},</li>
+                  )) : <li className="ml-2">N/A</li>
+                }
+              </ul>
+            </li>
+          </ul>
           <p className="text-sm">{character?.about}</p>
         </div>
       </div>
